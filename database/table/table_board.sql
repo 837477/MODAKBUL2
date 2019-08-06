0d450d56dcbe8c165d472f27ae117b1dc99cbebf
서정민 <seojeongmin@seojeongmin-ui-MacBookPro.local>
2019-08-06 23:30:47 +0900

    Modakbul update

--- database/table/table_board.sql
+++ database/table/table_board.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS board(
 board_url VARCHAR(20) NOT NULL,
+board_rank INT NOT NULL DEFAULT 0,
 board_name VARCHAR(100) NOT NULL,
 board_type TINYINT NOT NULL DEFAULT 0,
 PRIMARY KEY(board_url)
