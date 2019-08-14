4902f9feca5d1236d4868d80c0bbe742d912e4e7
837477 <37999795+837477@users.noreply.github.com>
2019-08-14 18:03:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import main, auth, board, vote, search, admin, analysis
 import error
 
 application = Flask(__name__, instance_relative_config=True)
-application.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
+application.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
 cors = CORS(application)
 
 #Debug or Release
